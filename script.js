bdb6dcb7da682a9a0ba8e3d733a711bbea43d9ca
kukharukdaria <kukharukdaria@gmail.com>
2019-05-09 21:53:33 -0600

    background edit

---
+++ script.js
@@ -0,0 +1,4 @@
+function wowMe(int){
+    document.body.style.backgroundImage = 'url(https://firebasestorage.googleapis.com/v0/b/cs260projects-e285c.appspot.com/o/Fireworks-13.jpg?alt=media&token=af2bfa20-8ebb-407a-939d-ec98a204bb63)';
+    document.body.style.color = '#FFFFFF';
+}
